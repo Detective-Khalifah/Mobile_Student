692d93aca3891c3172ecf4ef5ffba2b14f5fde7e
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-02-21 17:25:16 +0100

    Make NotesActivity the launcher activity -- Home
    THIS IS ONLY FOR TESTING PURPOSES. Will be back to MainActivity shortly.
    
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesActivity.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesActivity.java
@@ -1,7 +1,11 @@
 package com.blogspot.thengnet.mobilestudent;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class NotesActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            startActivity(new Intent(NotesActivity.this, MainActivity.class));
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openMediaActivity(View view) {
+        startActivity(new Intent(NotesActivity.this, MainActivity.class));
     }
 }
