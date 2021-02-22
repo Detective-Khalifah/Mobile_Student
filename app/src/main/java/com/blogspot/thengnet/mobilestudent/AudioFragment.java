796cb929ab4943749d49a0f7469d6102e66a9284
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-02-22 01:45:34 +0100

    Fix multiple audio files playing when clicked.
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/AudioFragment.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/AudioFragment.java
@@ -1,18 +1,22 @@
 package com.blogspot.thengnet.mobilestudent;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -20,15 +24,15 @@ import java.util.ArrayList;
  */
 public class AudioFragment extends Fragment {
 
+    private static Cursor audioCursor;
+    private static MediaPlayer mAudioPlayer;
+    private String LOG_TAG = AudioFragment.class.getName();
     private ArrayList<Audio> mAudioFiles;
     private String[] mAudioTableColumns;
     private String mAudioSelection = MediaStore.Audio.Media.IS_MUSIC + " OR " +
             MediaStore.Audio.Media.IS_PODCAST;
     private String mAudioSortOrder = MediaStore.Audio.Media.TITLE + " ASC"; // TODO: present option in preferences
     private Uri mAudioUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-
-    private static Cursor audioCursor;
-    private static MediaPlayer mAudioPlayer;
 
     public AudioFragment () {
         // Required empty public constructor
@@ -61,11 +65,11 @@ public class AudioFragment extends Fragment {
                 int nLength = audioCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
 
                 mAudioFiles.add(
-                    new Audio(
-                            audioCursor.getString(nTitle),
-                            audioCursor.getString(nPath),
-                            audioCursor.getString(nLength)
-                    )
+                        new Audio(
+                                audioCursor.getString(nTitle),
+                                audioCursor.getString(nPath),
+                                audioCursor.getString(nLength)
+                        )
                 );
             } while (audioCursor.moveToNext());
             audioCursor.close();
@@ -80,10 +84,7 @@ public class AudioFragment extends Fragment {
             @Override
             public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
                 Audio currentAudio = mAudioFiles.get(position);
-                mAudioPlayer = MediaPlayer.create(getContext(),
-                        Uri.parse(currentAudio.getAudioPath()
-                        ));
-                mAudioPlayer.start();
+                playAudioFile(getContext(), Uri.parse(currentAudio.getAudioPath()));
             }
         });
 
@@ -91,4 +92,27 @@ public class AudioFragment extends Fragment {
         return audioRoot;
     }
 
+    private boolean playAudioFile (Context context, Uri path) {
+        if (mAudioPlayer != null) {
+            Log.v(LOG_TAG, "mAudioPlayer!=null");
+            mAudioPlayer.reset();
+            try {
+                mAudioPlayer.setDataSource(context, path);
+                Log.v(LOG_TAG, "setDatasource");
+                mAudioPlayer.prepare();
+                Log.v(LOG_TAG, "prepare");
+                mAudioPlayer.start();
+                Toast.makeText(getContext(), "Started: " + path.toString(), Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+        Log.v(LOG_TAG, "mediaPlayer = null");
+        mAudioPlayer = MediaPlayer.create(context, path);
+        Log.v(LOG_TAG, "create");
+        mAudioPlayer.start();
+        Toast.makeText(getContext(), "Started: " + path.toString(), Toast.LENGTH_SHORT).show();
+        return false;
+    }
 }
