774bcb461ff2aade7179d41ad7a298a53fb1d030
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-02-25 03:09:28 +0100

    Test NotesCursorAdapter -- works
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesActivity.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
 import com.blogspot.thengnet.mobilestudent.data.NoteContract;
 import com.blogspot.thengnet.mobilestudent.data.NoteDbHelper;
@@ -20,25 +21,19 @@ public class NotesActivity extends AppCompatActivity {
 
     private ArrayList<Note> mNotes;
 
-    private NoteDbHelper mNoteDbHelper;
-
     private FloatingActionButton fabNewNote;
+
+    private NotesCursorAdapter mNoteCursorAdapter;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-//        mNotes = new ArrayList<>();
-//        mNotes.add(new Note("Second Note - from Activity",
-//                "This is a test note from NotesActivity. 0... 1... 2... 3... 4... 5....",
-//                new Date()));
-//
-//        mNotes.add(new Note("Third Note - from Activity",
-//                "This is a test note from NotesActivity. 2... 1... 0...",
-//                new Date()));
+        ListView notesList = (ListView) findViewById(R.id.notes_list);
 
-//        mNoteDbHelper = new NoteDbHelper(this);
+        mNoteCursorAdapter = new NotesCursorAdapter(this, getRows());
+        notesList.setAdapter(mNoteCursorAdapter);
 
         fabNewNote = (FloatingActionButton) findViewById(R.id.fab_new_note);
         fabNewNote.setOnClickListener(new View.OnClickListener() {
@@ -75,18 +70,7 @@ public class NotesActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void openMediaActivity (View view) {
-        if (getRows()) {
-
-//            NotesAdapter notesAdapter = new NotesAdapter(this, mNotes);
-//
-//            ListView notesList = (ListView) findViewById(R.id.notes_list);
-//            notesList.setAdapter(notesAdapter);
-        }
-//        startActivity(new Intent(NotesActivity.this, MainActivity.class));
-    }
-
-    private boolean getRows () {
+    private Cursor getRows () {
 
         Cursor existentNotes = getContentResolver().query(NoteContract.NoteEntry.CONTENT_URI, null, null, null, null);
         int totalRows = -2;
@@ -94,6 +78,7 @@ public class NotesActivity extends AppCompatActivity {
         if (existentNotes != null && totalRows != -1) {
             Snackbar.make(findViewById(R.id.notes_snackbar_frame), "Cursor not empty!", Snackbar.LENGTH_SHORT).show();
             Log.v(NotesActivity.class.getName(), "Cursor not empty!");
+
             totalRows = existentNotes.getCount();
             existentNotes.moveToFirst();
 
@@ -107,20 +92,13 @@ public class NotesActivity extends AppCompatActivity {
                 String noteDate = existentNotes.getString(date);
 
                 Log.v(NotesActivity.class.getName(), "Title: " + title + "\nContent: " + content + "Date: " + noteDate);
-//                mNotes.add(new Note(title, content, new Date(noteDate)));
                 existentNotes.moveToNext();
             }
-            return true;
+            return existentNotes;
         } else {
             Snackbar.make(findViewById(R.id.notes_snackbar_frame), "Cursor empty!", Snackbar.LENGTH_SHORT).show();
             Log.v(NotesActivity.class.getName(), "Cursor empty!");
-            return false;
+            return null;
         }
     }
 }
-
-//mNotes.add(new Note(
-//        existentNotes.getString(existentNotes.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_CONTENT)),
-//        existentNotes.getString(existentNotes.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_CONTENT)),
-//        new Date(existentNotes.getString(existentNotes.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_PREVIOUS_UPDATE))))
-//        );
