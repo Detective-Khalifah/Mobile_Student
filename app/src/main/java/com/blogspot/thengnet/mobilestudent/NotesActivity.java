adc0169e54b4cf881971b5effe8dbc6eedfc53db
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-03-03 21:56:16 +0100

    Add NotesFragment and change NotesActivity to transition between the fragments
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesActivity.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesActivity.java
@@ -1,93 +1,48 @@
 package com.blogspot.thengnet.mobilestudent;
 
-import android.content.ContentUris;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.Loader;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
 
-import com.blogspot.thengnet.mobilestudent.data.NoteContract;
-
-public class NotesActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-
-    ProgressBar pbLoadingNotes;
-
-    private FloatingActionButton fabNewNote;
-
-    private NotesCursorAdapter mNoteCursorAdapter;
-
-    private static final int NOTES_ACTIVITY_LOADER = 105;
+public class NotesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-        pbLoadingNotes = (ProgressBar) findViewById(R.id.pb_loading_notes);
-
-        ListView notesList = (ListView) findViewById(R.id.notes_list);
-
-        mNoteCursorAdapter = new NotesCursorAdapter(this, null);
-        notesList.setAdapter(mNoteCursorAdapter);
-
-        notesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
-                showProgress();
-                startActivity(new Intent(NotesActivity.this, NotesViewer.class)
-                        .setData(ContentUris.withAppendedId(
-                                NoteContract.NoteEntry.CONTENT_URI, id))); // content uri of the clicked #NoteEntry
-            }
-        });
-
-        getSupportLoaderManager().initLoader(NOTES_ACTIVITY_LOADER, null, this);
-
-        fabNewNote = (FloatingActionButton) findViewById(R.id.fab_new_note);
-        fabNewNote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                startActivity(new Intent(NotesActivity.this, NotesEditor.class));
-            }
-        });
-
         final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.getMenu().getItem(1).setChecked(true);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected (MenuItem menuItem) {
+                Fragment currentFragment = null;
                 switch (menuItem.getItemId()) {
-//                    Fragment currentFragment = null;
                     case R.id.media_page:
                         menuItem.setChecked(true);
-                        Fragment currentFragment = new MediaFragment();
+                        currentFragment = new MediaFragment();
                         FragmentTransaction mediaTransact = getSupportFragmentManager().beginTransaction();
-                        mediaTransact.replace(R.id.fragment_media, currentFragment);
+                        mediaTransact.replace(R.id.fragment_frame, currentFragment);
                         mediaTransact.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                         mediaTransact.commit();
 //                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         break;
                     case R.id.notes_page:
                         menuItem.setChecked(true);
-//                        FragmentTransaction ft = getSupportFragmentManager()
-//                                .beginTransaction();
-//                        ft.replace(R.id.details, details);
-//                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-//                        ft.commit();
-                        startActivity(new Intent(getApplicationContext(), NotesActivity.class));
+                        currentFragment = new NotesFragment().newInstance(null, null);
+                        FragmentTransaction ft = getSupportFragmentManager()
+                                .beginTransaction();
+                        ft.replace(R.id.fragment_frame, currentFragment);
+                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                        ft.commit();
+//                        startActivity(new Intent(getApplicationContext(), NotesActivity.class));
                         break;
                     case R.id.calculator_page:
+                        currentFragment = null;
                         break;
                 }
                 return false;
@@ -95,44 +50,4 @@ public class NotesActivity extends AppCompatActivity implements LoaderManager.Lo
         });
     }
 
-    @Override
-    public boolean onOptionsItemSelected (MenuItem item) {
-//        if (item.getItemId() == android.R.id.home)
-//            startActivity(new Intent(NotesActivity.this, MainActivity.class));
-        return super.onOptionsItemSelected(item);
-    }
-
-    public Loader<Cursor> onCreateLoader (int i, Bundle bundle) {
-        String[] projection = {
-                NoteContract.NoteEntry._ID,
-                NoteContract.NoteEntry.COLUMN_NOTE_TITLE,
-                NoteContract.NoteEntry.COLUMN_NOTE_CONTENT,
-                NoteContract.NoteEntry.COLUMN_NOTE_PREVIOUS_UPDATE
-        };
-        String whereClause = null;
-        String[] whereArgs = null;
-
-        showProgress();
-        return new CursorLoader(this, NoteContract.NoteEntry.CONTENT_URI,
-                projection, whereClause, whereArgs, null);
-    }
-
-    @Override
-    public void onLoadFinished (Loader<Cursor> loader, Cursor cursor) {
-        hideProgress();
-        mNoteCursorAdapter.swapCursor(cursor);
-    }
-
-    @Override
-    public void onLoaderReset (Loader<Cursor> loader) {
-        mNoteCursorAdapter.swapCursor(null);
-    }
-
-    private void showProgress () {
-        pbLoadingNotes.setVisibility(View.VISIBLE);
-    }
-
-    private void hideProgress () {
-        pbLoadingNotes.setVisibility(View.GONE);
-    }
 }
---
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesFragment.java
@@ -0,0 +1,142 @@
+package com.blogspot.thengnet.mobilestudent;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.Loader;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.blogspot.thengnet.mobilestudent.data.NoteContract;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NotesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final int NOTES_ACTIVITY_LOADER = 105;
+    ProgressBar pbLoadingNotes;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private FloatingActionButton fabNewNote;
+    private NotesCursorAdapter mNoteCursorAdapter;
+
+    public NotesFragment () {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NotesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NotesFragment newInstance (String param1, String param2) {
+        NotesFragment fragment = new NotesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        getLoaderManager().initLoader(NOTES_ACTIVITY_LOADER, null, this);
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View notesView = inflater.inflate(R.layout.fragment_notes, container, false);
+
+        pbLoadingNotes = (ProgressBar) notesView.findViewById(R.id.pb_loading_notes);
+
+        fabNewNote = (FloatingActionButton) notesView.findViewById(R.id.fab_new_note);
+        fabNewNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+//                startActivity(new Intent(NotesActivity.this, NotesEditor.class));
+            }
+        });
+
+        ListView notesList = (ListView) notesView.findViewById(R.id.notes_list);
+
+        mNoteCursorAdapter = new NotesCursorAdapter(getContext(), null);
+        notesList.setAdapter(mNoteCursorAdapter);
+
+        notesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                showProgress();
+//                startActivity(new Intent(NotesFragment.this, NotesViewer.class)
+//                        .setData(ContentUris.withAppendedId(
+//                                NoteContract.NoteEntry.CONTENT_URI, id))); // content uri of the clicked #NoteEntry
+            }
+        });
+
+        return notesView;
+    }
+
+    public Loader<Cursor> onCreateLoader (int i, Bundle bundle) {
+        String[] projection = {
+                NoteContract.NoteEntry._ID,
+                NoteContract.NoteEntry.COLUMN_NOTE_TITLE,
+                NoteContract.NoteEntry.COLUMN_NOTE_CONTENT,
+                NoteContract.NoteEntry.COLUMN_NOTE_PREVIOUS_UPDATE
+        };
+        String whereClause = null;
+        String[] whereArgs = null;
+
+        showProgress();
+        return new CursorLoader(getContext(), NoteContract.NoteEntry.CONTENT_URI,
+                projection, whereClause, whereArgs, null);
+    }
+
+    @Override
+    public void onLoadFinished (Loader<Cursor> loader, Cursor cursor) {
+        hideProgress();
+        mNoteCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset (Loader<Cursor> loader) {
+        mNoteCursorAdapter.swapCursor(null);
+    }
+
+    private void showProgress () {
+        pbLoadingNotes.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgress () {
+        pbLoadingNotes.setVisibility(View.GONE);
+    }
+}
