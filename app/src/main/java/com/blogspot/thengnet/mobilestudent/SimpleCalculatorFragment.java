69901471ae901f6a26380dd018cb27b9b3438ae9
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-03-12 20:57:37 +0100

    Add the Simple Calculator Fragment
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

---
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/SimpleCalculatorFragment.java
@@ -0,0 +1,30 @@
+package com.blogspot.thengnet.mobilestudent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SimpleCalculatorFragment extends Fragment {
+
+    public SimpleCalculatorFragment () {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simple_calculator, container, false);
+    }
+}
