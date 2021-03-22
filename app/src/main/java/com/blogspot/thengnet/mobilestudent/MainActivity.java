2202042c3a01ee52b0b426abe992f9e15d4ac0cb
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-03-22 20:18:43 +0100

    Display #curentFragment and hide others -- in case of "currentFragment = notesFrag", hide the calcFrag & mediaFrag
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/MainActivity.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/MainActivity.java
@@ -9,8 +9,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Fragment currentFragment, // the current activity selected
-            mediaFrag, notesFrag, calcFrag;
+    Fragment currentFragment, // the current activity selected
+            mediaFrag, calcFrag, notesFrag;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -27,12 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Replace #FrameLayout content with {@link NotesFragment} at startup
         currentFragment = notesFrag;
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_frame, currentFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_frame, currentFragment).commit();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected (MenuItem menuItem) {
-                Fragment currentFragment = null;
                 switch (menuItem.getItemId()) {
                     case R.id.media_page:
                         menuItem.setChecked(true);
@@ -57,36 +56,58 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayCalculatorFrag () {
         FragmentTransaction calculatorFragTransaction = getSupportFragmentManager().beginTransaction();
+        calculatorFragTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
         if (currentFragment.isAdded()) {
             calculatorFragTransaction.show(currentFragment);
-            calculatorFragTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         } else {
             calculatorFragTransaction.add(R.id.fragment_frame, currentFragment);
         }
 
+        if (mediaFrag.isAdded())
+            calculatorFragTransaction.hide(mediaFrag);
+        if (notesFrag.isAdded())
+            calculatorFragTransaction.hide(notesFrag);
+
+        // commit changes made on {@link FragmentTransaction} #calculatorFragTransaction
         calculatorFragTransaction.commit();
     }
 
     private void displayNotesFrag () {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
         if (currentFragment.isAdded()) {
             ft.show(currentFragment);
-            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         } else {
             ft.add(R.id.fragment_frame, currentFragment);
         }
 
+        if (calcFrag.isAdded())
+            ft.hide(calcFrag);
+        if (mediaFrag.isAdded())
+            ft.hide(mediaFrag);
+
+        // commit changes made on {@link FragmentTransaction} #ft
         ft.commit();
     }
 
     private void displayMediaFrag () {
         FragmentTransaction mediaTransact = getSupportFragmentManager().beginTransaction();
+        mediaTransact.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
         if (currentFragment.isAdded()) {
             mediaTransact.show(currentFragment);
         } else {
             mediaTransact.add(R.id.fragment_frame, currentFragment);
-            mediaTransact.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         }
+
+        if (calcFrag.isAdded())
+            mediaTransact.hide(calcFrag);
+        if (notesFrag.isAdded())
+            mediaTransact.hide(notesFrag);
+
+        // commit changes made on {@link FragmentTransaction} #mediaTransact
         mediaTransact.commit();
     }
 
