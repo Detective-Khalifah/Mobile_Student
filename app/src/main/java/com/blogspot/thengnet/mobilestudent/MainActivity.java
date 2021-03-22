8b8f342e755abfcdba9dfd6669932037ae5cdcca
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-03-22 19:32:20 +0100

    Extract fragment-replacing statements from each case of the BottomNavigationView into a method -- #displayCalculatorFrag(), #displayNotesFrag(), #displayMediaFrag()
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/MainActivity.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.getMenu().getItem(1).setChecked(true);
 
@@ -27,33 +28,48 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.media_page:
                         menuItem.setChecked(true);
-                        currentFragment = new MediaFragment();
-                        FragmentTransaction mediaTransact = getSupportFragmentManager()
-                                .beginTransaction();
-                        mediaTransact.replace(R.id.fragment_frame, currentFragment);
-                        mediaTransact.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                        mediaTransact.commit();
+                        displayMediaFrag();
                         break;
                     case R.id.notes_page:
                         menuItem.setChecked(true);
-                        currentFragment = new NotesFragment();
-                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                        ft.replace(R.id.fragment_frame, currentFragment);
-                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                        ft.commit();
+                        displayNotesFrag();
                         break;
                     case R.id.calculator_page:
                         menuItem.setChecked(true);
-                        currentFragment = new SimpleCalculatorFragment();
-                        FragmentTransaction calculatorFragTransaction = getSupportFragmentManager().beginTransaction();
-                        calculatorFragTransaction.replace(R.id.fragment_frame, currentFragment);
-                        calculatorFragTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                        calculatorFragTransaction.commit();
+                        displayCalculatorFrag();
                         break;
                 }
                 return false;
             }
         });
+    }
+
+    private void displayCalculatorFrag () {
+        Fragment currentFragment;
+        currentFragment = new SimpleCalculatorFragment();
+        FragmentTransaction calculatorFragTransaction = getSupportFragmentManager().beginTransaction();
+        calculatorFragTransaction.replace(R.id.fragment_frame, currentFragment);
+        calculatorFragTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        calculatorFragTransaction.commit();
+    }
+
+    private void displayNotesFrag () {
+        Fragment currentFragment;
+        currentFragment = new NotesFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_frame, currentFragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+    }
+
+    private void displayMediaFrag () {
+        Fragment currentFragment;
+        currentFragment = new MediaFragment();
+        FragmentTransaction mediaTransact = getSupportFragmentManager()
+                .beginTransaction();
+        mediaTransact.replace(R.id.fragment_frame, currentFragment);
+        mediaTransact.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        mediaTransact.commit();
     }
 
 }
