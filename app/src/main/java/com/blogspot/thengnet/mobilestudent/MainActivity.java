68d3a612bd435cb34d6eb36ee9d79de06b49d66b
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-03-22 20:46:54 +0100

    Fix -- clicking "Media" {@link BottomNavigationView} Menu Item multiple times makes media items disappear Comment {@link MainActivity} code
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/MainActivity.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/MainActivity.java
@@ -27,11 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Replace #FrameLayout content with {@link NotesFragment} at startup
         currentFragment = notesFrag;
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_frame, currentFragment).commit();
+        displayNotesFrag();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected (MenuItem menuItem) {
+                // set the #currentFragment to {@link BottomNavigationView} item selected and display it
+
                 switch (menuItem.getItemId()) {
                     case R.id.media_page:
                         menuItem.setChecked(true);
@@ -54,16 +56,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Use a {@link FragmentTransaction} to add/show the {@link SimpleCalculatorFragment} and hide
+     * other 2 fragments -- {@link MediaFragment} & {@link NotesFragment}.
+     */
     private void displayCalculatorFrag () {
         FragmentTransaction calculatorFragTransaction = getSupportFragmentManager().beginTransaction();
         calculatorFragTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
 
+        // Make the #SimpleCalculatorFragment visible
         if (currentFragment.isAdded()) {
             calculatorFragTransaction.show(currentFragment);
         } else {
             calculatorFragTransaction.add(R.id.fragment_frame, currentFragment);
         }
 
+        // Hide the Media and Notes Fragments
         if (mediaFrag.isAdded())
             calculatorFragTransaction.hide(mediaFrag);
         if (notesFrag.isAdded())
@@ -73,16 +81,22 @@ public class MainActivity extends AppCompatActivity {
         calculatorFragTransaction.commit();
     }
 
+    /**
+     * Use a {@link FragmentTransaction} to add/show the {@link NotesFragment} and hide
+     * other 2 fragments -- {@link SimpleCalculatorFragment} & {@link MediaFragment}.
+     */
     private void displayNotesFrag () {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
 
+        // Make the #NotesFragment visible
         if (currentFragment.isAdded()) {
             ft.show(currentFragment);
         } else {
             ft.add(R.id.fragment_frame, currentFragment);
         }
 
+        // Hide the Simple Calculator and Media Fragments
         if (calcFrag.isAdded())
             ft.hide(calcFrag);
         if (mediaFrag.isAdded())
@@ -92,16 +106,22 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
     }
 
+    /**
+     * Use a {@link FragmentTransaction} to add/show the {@link MediaFragment} and hide
+     * other 2 fragments -- {@link SimpleCalculatorFragment} & {@link NotesFragment}.
+     */
     private void displayMediaFrag () {
         FragmentTransaction mediaTransact = getSupportFragmentManager().beginTransaction();
         mediaTransact.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
 
+        // Make the #MediaFragment visible
         if (currentFragment.isAdded()) {
             mediaTransact.show(currentFragment);
         } else {
             mediaTransact.add(R.id.fragment_frame, currentFragment);
         }
 
+        // Hide the Simple Calculator & Notes Fragments
         if (calcFrag.isAdded())
             mediaTransact.hide(calcFrag);
         if (notesFrag.isAdded())
