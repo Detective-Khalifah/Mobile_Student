f97a73aa4db54751c267a73d45f20b47fd32b84e
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-03-29 12:30:29 +0100

    Use #getIntent().getData() to ascertain an already saved note is being edited; fetch details of the note and set to the 2 {@link TextInputEditText}s -- #editTitle, #editContent. Otherwise, let the user edit a new note.
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesEditor.java
+++ app/src/main/java/com/blogspot/thengnet/mobilestudent/NotesEditor.java
@@ -1,11 +1,15 @@
 package com.blogspot.thengnet.mobilestudent;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
 import android.net.Uri;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,6 +18,8 @@ import com.blogspot.thengnet.mobilestudent.data.NoteContract;
 import java.util.Date;
 
 public class NotesEditor extends AppCompatActivity {
+
+    private static Uri mNoteUri;
 
     TextInputEditText editTitle, editContent;
 
@@ -25,6 +31,14 @@ public class NotesEditor extends AppCompatActivity {
         editTitle = (TextInputEditText) findViewById(R.id.edit_note_title);
         editContent = (TextInputEditText) findViewById(R.id.edit_note_content);
 
+        Intent currentNote = getIntent();
+        mNoteUri = currentNote.getData();
+
+        // Fetch the saved note's details if its Uri was passed; do not fetch details of a note
+        // that does not exist yet, otherwise!
+        if (mNoteUri != null) {
+            getNoteDetails();
+        }
     }
 
     @Override
@@ -48,7 +62,38 @@ public class NotesEditor extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    private void saveNote() {
+    private void getNoteDetails () {
+        Cursor currentNote = null;
+        Snackbar notFound = null;
+        try {
+            currentNote = getContentResolver().query(
+                    mNoteUri, null, null, null, null);
+
+            if (currentNote != null) {
+                currentNote.moveToFirst();
+                Log.v(NotesEditor.class.getName(), "Note URI: " + mNoteUri + "\n" + "CurrentNote: " + currentNote);
+
+                editTitle.setText(currentNote.getString(currentNote.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_TITLE)));
+                editContent.setText(currentNote.getString(currentNote.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_CONTENT)));
+            } else
+                throw new CursorIndexOutOfBoundsException("Could not find note!");
+        } catch (CursorIndexOutOfBoundsException cursorException) {
+            // TODO: Show a Snackbar only if one is not currently being shown.
+            if (notFound == null) {
+                notFound = Snackbar.make(findViewById(R.id.editor_snackbar_frame),
+                        cursorException.getMessage() != null ? cursorException.getMessage() : "",
+                        Snackbar.LENGTH_LONG);
+                notFound.show();
+            }
+
+            // TODO: Set a timer to close - #finish() - this activity after 30 seconds.
+        } finally {
+            if (currentNote != null)
+                currentNote.close();
+        }
+    }
+
+    private void saveNote () {
         String noteTitle = editTitle.getText().toString().trim();
         String noteContent = editContent.getText().toString();
 
